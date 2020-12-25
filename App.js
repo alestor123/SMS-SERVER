7909b3269a95fa9dd47726d9152f9b5f2d1fe4fd
alestor123 <alestoraldous@gmail.com>
2020-12-25 12:45:56 +0530

    added logger

--- App.js
+++ App.js
@@ -11,3 +11,35 @@ pck = require('./package.json'),
 chalk = require('chalk');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+app.get('/github', (req,res) => {
+	res.redirect(pck.homepage)
+	logger.req('Redirect',req)
+})
+
+
+
+
+
+
+// Logger
+function logger(message){
+    console.log(chalk.bgYellow.red(`(LOG):${Date()}:${message}`))
+    fsLog(message)
+    }
+logger.req = (message,req) => {
+    console.log(chalk.bgYellow.blue(`(REQUEST):${Date()}:Ip : ${req.ip} : ${message}`))
+    fsLog(message)
+    }
+    logger.err = (message) => {
+    console.error(chalk.bgRed.green(`(ERROR):${Date()} : ${message}`))
+    fsLog(message)
+    }
+    // Main 
+    // file logging 
+    function fsLog(logText) {
+        if(options.fslog || options.f ||  process.env.LOG || false ){
+        fs.appendFile(options.fsLog || options.f ||  process.env.LOGPATH || 'logs.log' ,`\n ${logText} \n` , (err) => {
+            if (err) throw err;
+          });
+            }}
