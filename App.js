6be79b79a89527d853ab43fc67f811e0bac07318
alestor123 <alestoraldous@gmail.com>
2020-12-25 15:28:55 +0530

    logging token

--- App.js
+++ App.js
@@ -15,7 +15,34 @@ chalk = require('chalk');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+if(options.v || options.version){
+    console.log( `${pck.version}`)
+  process.exit(1);
+}
+else if (options.h || options.help) { // checking undifined args
+    console.log(`
+	Usage: ${pck.name} -p <Port Number> -t <Token> -l <Limit Number> -f <file path>
+    -t , --token    for setting tokn
+    -s , --sid    for setting tokn
+	-p , --port setting port number
+	-v , --version for showing cli version
+	-i , --issue for reporting web page (any issue or bugs)
+	-f , --fsLog for setting path for log file by default this option is not true 
+`);
+process.exit(0)
 
+}
+else if (options.i || options.issue) { // checking undifined args
+  console.log(`
+  Issues at ${pck.bugs.url} 
+`);
+process.exit(0)
+}
+
+else{
+logger(`SID : ${accountSid} , TOKEN : ${authToken} KEY: ${key}`)
+app.listen(port, () => logger(`Server running at ${port}`))
+}
 app.get('/github', (req,res) => {
 	res.redirect(pck.homepage)
 	logger.req('Redirect',req)
@@ -40,8 +67,6 @@ else {
 })
 
 
-
-app.listen(port, () => logger(`Server running at ${port}`))
 
 
 // Logger
