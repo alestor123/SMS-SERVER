7e2111e22a31676075149e2d64af8c871057342a
alestor123 <alestoraldous@gmail.com>
2020-12-25 12:41:10 +0530

    added options

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 require('dotenv').config()
-
 var express = require('express'),
 app = express(),
+options  = require('minimist')(process.argv.slice(2));
+path = require('path'),
+pck = require('./package.json'),
 chalk = require('chalk');
